6ab45a7d452e34e8e967ca1c00c19152e2081626
eDetektor <detektor.bis@gmail.com>
2025-07-29 20:43:04 +0200

    Add files via upload
    Remember change chmod 700

---
+++ install_mullvad_vpn_apt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e  # przerwij w razie błędu
+
+# Download the Mullvad signing key
+curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+
+# Add the Mullvad repository
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$(dpkg --print-architecture)] https://repository.mullvad.net/deb/stable stable main" > /etc/apt/sources.list.d/mullvad.list
+
+# Update and install
+apt update
+apt install -y mullvad-vpn
+
