8528976abbf600cda82960ac5de09cebf3aed11a
eDetektor <detektor.bis@gmail.com>
2025-08-14 11:59:46 +0200

    Add files via upload

--- install_mullvad_vpn_apt.sh
+++ install_mullvad_vpn_apt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # abort on error
-set -e  
+set -e
 
 sudo apt install -y curl
 
 # Download the Mullvad signing key
-curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
 
 # Add the Mullvad repository
 echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$(dpkg --print-architecture)] https://repository.mullvad.net/deb/stable stable main" > /etc/apt/sources.list.d/mullvad.list
